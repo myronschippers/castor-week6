492e73fa72a934a9341bb9dcc7da5ed66be1f48c
Myron <dubtwee@gmail.com>
2020-10-01 18:56:17 -0500

    adding code examples from lecture

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,41 @@
 console.log('Hello Castor');
+
+const fruitBasketList = [];
+
+$(document).ready(pageReady);
+
+function pageReady() {
+  console.log('READY');
+
+  // access to DOM
+  // EVENT LISTENER
+  $('#jsAddFruit').on('click', clickAddFruitHandler)
+}
+
+// EVENT HANDLER
+function clickAddFruitHandler() {
+  // capture the entered form field value
+  const fruitName = $('#jsFruit').val();
+
+  addFruitToBasket(fruitName);
+  render();
+}
+
+function addFruitToBasket(nameOfFruit) {
+  const fruitInfo = {
+    name: nameOfFruit,
+  };
+  // push fruit in bascket
+  fruitBasketList.push(fruitInfo);
+}
+
+function render() {
+  const $basket = $('#jsBasket');
+
+  $basket.empty();
+  for (let i = 0; i < fruitBasketList.length; i++) {
+    const fruit = fruitBasketList[i];
+    // update DOM with fruit
+    $basket.append(`<li>${fruit.name}</li>`);
+  }
+}
